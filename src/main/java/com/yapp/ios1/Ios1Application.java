358bfafab73fa499fe20d38217c1f956c7d53d5f
ayoung0073 <ayong0310@naver.com>
2021-02-24 15:55:11 +0900

    [YITEAM-7] Project setting

---
+++ src/main/java/com/yapp/ios1/Ios1Application.java
@@ -0,0 +1,13 @@
+package com.yapp.ios1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ios1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ios1Application.class, args);
+    }
+
+}
