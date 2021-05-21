4ca53e8cb68714da79df7e3bda3f023cb31ac970
Gyunny <wjdrbs966@naver.com>
2021-05-21 16:01:39 +0900

    [YITEAM-72] 설명 주석 추가

--- src/main/java/com/yapp/ios1/dto/follow/FriendStatus.java
+++ src/main/java/com/yapp/ios1/dto/follow/FriendStatus.java
@@ -4,9 +4,9 @@ package com.yapp.ios1.dto.follow;
  * created by jg 2021/05/21
  */
 public enum FriendStatus {
-    FRIEND(1),
-    REQUEST(2),
-    NOT_FRIEND(3);
+    FRIEND(1),      // 친구
+    REQUEST(2),     // 요청 중
+    NOT_FRIEND(3);  // 친구 아님
 
     private final int friendStatus;
 
--- src/main/java/com/yapp/ios1/dto/notification/AlarmLogStatus.java
+++ src/main/java/com/yapp/ios1/dto/notification/AlarmLogStatus.java
@@ -4,8 +4,8 @@ package com.yapp.ios1.dto.notification;
  * created by jg 2021/05/21
  */
 public enum AlarmLogStatus {
-    ACTIVITY(1),
-    NO_ACTIVITY(2);
+    ACTIVITY(1),     // 알람 활성화
+    NO_ACTIVITY(2);  // 알람 삭제됨
 
     private final int alarmStatus;
 
