5b60824b659d1d93666c369cb49b2386d2ba2f1f
ayoung0073 <ayong0310@naver.com>
2021-06-02 11:51:30 +0900

    [YITEAM-118] 버킷 요청 데이터 유효성 검사 추가

--- src/main/java/com/yapp/ios1/dto/bucket/BucketRequestDto.java
+++ src/main/java/com/yapp/ios1/dto/bucket/BucketRequestDto.java
@@ -3,8 +3,15 @@ package com.yapp.ios1.dto.bucket;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDate;
 import java.util.List;
+
+import static com.yapp.ios1.common.ResponseMessage.*;
 
 /**
  * created by ayoung 2021/05/08
@@ -17,11 +24,16 @@ public class BucketRequestDto {
     private Long id;
     @JsonIgnore
     private Long userId;
+    @NotBlank(message = NOT_VALID_BUCKET_NAME)
     private String bucketName;
+    @Min(value = 2, message = NOT_VALID_STATE)
+    @Max(value = 4, message = NOT_VALID_STATE)
     private int state;
+    @Min(value = 2, message = NOT_VALID_CATEGORY_ID)
+    @Max(value = 10, message = NOT_VALID_CATEGORY_ID)
     private int categoryId;
-    private String startDate;
-    private String endDate;
+    private LocalDate startDate;
+    private LocalDate endDate;
     private String content;
     private List<String> imageList;
     private List<String> tagList;
