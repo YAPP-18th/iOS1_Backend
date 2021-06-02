4ef9cadcfd85ecf85c0f82b0ff15d38366a60b6c
Gyunny <wjdrbs966@naver.com>
2021-06-03 00:27:40 +0900

    [YITEAM-120] ErrorCode & ErrorResponse 정의

---
+++ src/main/java/com/yapp/ios1/error/exception/ErrorCode.java
@@ -0,0 +1,49 @@
+package com.yapp.ios1.error.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+/**
+ * created by jg 2021/06/03
+ */
+@JsonFormat(shape = JsonFormat.Shape.OBJECT) // enum 에서 key-value 쌍을 사용할 때
+public enum ErrorCode {
+
+    // Common
+    INVALID_INPUT_VALUE(400, "C001", " Invalid Input Value"),
+    METHOD_NOT_ALLOWED(405, "C002", " Invalid Input Value"),
+    ENTITY_NOT_FOUND(400, "C003", " Entity Not Found"),
+    INTERNAL_SERVER_ERROR(500, "C004", "Server Error"),
+    INVALID_TYPE_VALUE(400, "C005", " Invalid Type Value"),
+    HANDLE_ACCESS_DENIED(403, "C006", "Access is Denied"),
+
+    // User
+    EMAIL_DUPLICATION(400, "M001", "Email is Duplication"),
+    LOGIN_INPUT_INVALID(400, "M002", "Login input is invalid"),
+
+    // Coupon
+    COUPON_ALREADY_USE(400, "CO001", "Coupon was already used"),
+    COUPON_EXPIRE(400, "CO002", "Coupon was already expired")
+    ;
+
+    private final String code;
+    private final String message;
+    private int status;
+
+    ErrorCode(final int status, final String code, final String message) {
+        this.status = status;
+        this.message = message;
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
