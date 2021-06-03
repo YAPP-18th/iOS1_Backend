eee0d45a9cd02aa237a858fbd893e69cdba90fae
Gyunny <wjdrbs966@naver.com>
2021-06-03 23:39:44 +0900

    [YITEAM-120] APiIgnore 적용

--- src/main/java/com/yapp/ios1/controller/HealthController.java
+++ src/main/java/com/yapp/ios1/controller/HealthController.java
@@ -3,6 +3,7 @@ package com.yapp.ios1.controller;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 /**
  * created by jg 2021/06/02
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
 
     // 로드 밸런서 용
-    @ApiOperation("로드밸런서 용(사용 X)")
+    @ApiIgnore
     @GetMapping("/health")
     public String loadBalancer() {
         return "health";
