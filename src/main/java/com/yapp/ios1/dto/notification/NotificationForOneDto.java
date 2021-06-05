1099165c7c5c9e2d644be8e1d86db0f33f19bfff
Gyunny <wjdrbs966@naver.com>
2021-06-05 12:02:28 +0900

    [YITEAM-123] 컨벤션 수정

--- src/main/java/com/yapp/ios1/dto/notification/NotificationForOneDto.java
+++ src/main/java/com/yapp/ios1/dto/notification/NotificationForOneDto.java
@@ -2,14 +2,12 @@ package com.yapp.ios1.dto.notification;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 /**
  * created by jg 2021/05/21
  */
-@ToString
 @Getter
 public class NotificationForOneDto extends NotificationDto {
 
