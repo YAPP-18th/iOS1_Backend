ce8475f1ca813eda448ab5e57bf16f5d93225a40
Gyunny <wjdrbs966@naver.com>
2021-06-06 16:15:17 +0900

    [YITEAM-141] 버킷 홈 화면 content 추가

--- src/main/java/com/yapp/ios1/dto/bucket/BucketDto.java
+++ src/main/java/com/yapp/ios1/dto/bucket/BucketDto.java
@@ -1,14 +1,17 @@
 package com.yapp.ios1.dto.bucket;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 
 /**
  * created by jg 2021/05/05
  */
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
 public class BucketDto {
     private Long id;
     private String bucketName;
+    private String content;
     private String createdDate;
     private String endDate;
     private int bucketState;
--- src/main/java/com/yapp/ios1/dto/bucket/TagDto.java
+++ src/main/java/com/yapp/ios1/dto/bucket/TagDto.java
@@ -1,11 +1,13 @@
 package com.yapp.ios1.dto.bucket;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 /**
  * created by ayoung 2021/05/08
  */
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @NoArgsConstructor
 @Getter
 public class TagDto {
