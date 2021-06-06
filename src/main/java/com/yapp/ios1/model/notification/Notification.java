1fb385c9d5b7f09ee320bfa8fb6372f1e5a299a2
Gyunny <wjdrbs966@naver.com>
2021-06-07 01:19:13 +0900

    [YITEAM-147] 알람 로그 Refactor

--- src/main/java/com/yapp/ios1/model/notification/Notification.java
+++ src/main/java/com/yapp/ios1/model/notification/Notification.java
@@ -1,17 +1,19 @@
-package com.yapp.ios1.dto.notification.response;
+package com.yapp.ios1.model.notification;
 
 import lombok.Getter;
 
 /**
  * created by jg 2021/05/26
+ * 알림 로그 모델
  */
 @Getter
-public class NotificationLogResultDto {
+public class Notification {
     private Long alarmId;
     private Long myUserId;
     private Long friendId;
     private String title;
     private String message;
+    private String profileUrl;
     private Integer friendStatus;
     private String createdAt;
 }
