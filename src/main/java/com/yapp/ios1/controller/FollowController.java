ce6a1b6a3ab8836477851e41cf8ece334807b1b3
ayoung0073 <ayong0310@naver.com>
2021-06-07 00:42:16 +0900

    [YITEAM-146] Valid 메세지 ErrorCode로 이동

--- src/main/java/com/yapp/ios1/controller/FollowController.java
+++ src/main/java/com/yapp/ios1/controller/FollowController.java
@@ -1,7 +1,7 @@
 package com.yapp.ios1.controller;
 
 import com.yapp.ios1.dto.ResponseDto;
-import com.yapp.ios1.dto.user.result.FriendDto;
+import com.yapp.ios1.model.user.Friend;
 import com.yapp.ios1.service.FollowService;
 import com.yapp.ios1.utils.auth.Auth;
 import com.yapp.ios1.utils.auth.UserContext;
@@ -50,7 +50,7 @@ public class FollowController {
     @ApiOperation(value = "친구 리스트")
     @GetMapping("/users/{userId}/friends")
     public ResponseEntity<ResponseDto> getFriendList(@PathVariable Long userId) {
-        List<FriendDto> friendList = followService.getFriendList(userId);
+        List<Friend> friendList = followService.getFriendList(userId);
         return ResponseEntity.ok(ResponseDto.of(HttpStatus.OK, GET_FRIEND_LIST, friendList));
     }
 }
