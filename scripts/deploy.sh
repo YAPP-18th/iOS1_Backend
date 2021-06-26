1e9300889f4bf5f09b8b4caa47dc1bea8de6cb6c
Gyunny <wjdrbs966@naver.com>
2021-06-26 23:42:41 +0900

    [YITEAM-163] Docker rm error Solve

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,8 +10,8 @@ then
 else
   echo "> docker stop ${CONTAINER_ID}"
   sudo docker stop ${CONTAINER_ID}
-  # echo "> docker rm ${CONTAINER_ID}"
-  #sudo docker rm ${CONTAINER_ID}
+  echo "> docker rm ${CONTAINER_ID}"
+  sudo docker rm ${CONTAINER_ID}
   #sudo docker rmi yapp
   sleep 5
 fi
