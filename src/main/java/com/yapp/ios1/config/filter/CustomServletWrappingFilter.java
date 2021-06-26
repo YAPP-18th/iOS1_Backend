ecc4edd980914b43e34a3d07769e8a97b6a39bba
Gyunny <wjdrbs966@naver.com>
2021-06-27 02:31:21 +0900

    [YITEAM-165] Slack Error log 연결

---
+++ src/main/java/com/yapp/ios1/config/filter/CustomServletWrappingFilter.java
@@ -0,0 +1,30 @@
+package com.yapp.ios1.config.filter;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * created by jg 2021/06/27
+ */
+@Component
+public class CustomServletWrappingFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain) throws ServletException, IOException {
+        ContentCachingRequestWrapper wrappingRequest = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper wrappingResponse = new ContentCachingResponseWrapper(response);
+
+        chain.doFilter(wrappingRequest, wrappingResponse);
+
+        wrappingResponse.copyBodyToResponse();
+    }
+}
---
+++ src/main/java/com/yapp/ios1/config/interceptor/InterceptorConfig.java
@@ -0,0 +1,22 @@
+package com.yapp.ios1.config.interceptor;
+
+import com.yapp.ios1.config.interceptor.LoggingInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * created by jg 2021/06/27
+ */
+@RequiredArgsConstructor
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    private final LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
+}
