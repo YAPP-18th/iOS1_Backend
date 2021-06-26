9086015517b0b3a947851a345b6703dd15c5b7ac
Gyunny <wjdrbs966@naver.com>
2021-06-27 03:06:57 +0900

    [YITEAM-165] Spring Security Swagger 설정 수정

--- src/main/java/com/yapp/ios1/config/SecurityConfig.java
+++ src/main/java/com/yapp/ios1/config/SecurityConfig.java
@@ -23,8 +23,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/api/v2/*", "/health", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
-                        "/swagger-resources/**", "/webjars/**", "/swagger/**", "/v2/api-docs", "/configuration/**").permitAll()
+                .antMatchers("/api/v2/*", "/health", "/swagger-ui.html", "/swagger/**",
+                        "/swagger-resources/**", "/webjars/**", "/v2/api-docs").permitAll()
                 .anyRequest().authenticated()
                 .and()
                     .csrf().disable()
