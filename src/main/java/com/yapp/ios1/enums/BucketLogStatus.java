2874af1ff76eda581b1d1b61e3963668696db171
ayoung0073 <ayong0310@naver.com>
2021-06-28 11:21:42 +0900

    [YITEAM-137] 주석 정렬

--- src/main/java/com/yapp/ios1/enums/BucketLogStatus.java
+++ src/main/java/com/yapp/ios1/enums/BucketLogStatus.java
@@ -4,7 +4,7 @@ package com.yapp.ios1.enums;
  * created by ayoung 2021/06/28
  */
 public enum BucketLogStatus {
-    BUCKET_NAME_LOG(1),      // 버킷 이름 로그
+    BUCKET_NAME_LOG(1),         // 버킷 이름 로그
     BUCKET_END_DATE_LOG(2)      // 버킷 완료 날짜 로그
     ;
     private final int bucketLogStatus;
