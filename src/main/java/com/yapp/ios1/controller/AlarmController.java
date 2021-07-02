54195f93484c2b89cfbd271c4b9e808ee71ec864
ayoung0073 <ayong0310@naver.com>
2021-07-02 16:09:35 +0900

    [YITEAM-170] 푸시 알림 테스트 API 구현

--- src/main/java/com/yapp/ios1/controller/AlarmController.java
+++ src/main/java/com/yapp/ios1/controller/AlarmController.java
@@ -1,9 +1,12 @@
 package com.yapp.ios1.controller;
 
 import com.yapp.ios1.dto.ResponseDto;
+import com.yapp.ios1.dto.notification.NotificationForOneDto;
+import com.yapp.ios1.dto.notification.NotificationTestDto;
 import com.yapp.ios1.service.alarm.AlarmService;
 import com.yapp.ios1.annotation.Auth;
 import com.yapp.ios1.aop.UserContext;
+import com.yapp.ios1.service.alarm.FirebaseService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -20,14 +23,15 @@ import static com.yapp.ios1.message.ResponseMessage.GET_ALARM_LOG;
 @Api(tags = "Alarm")
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v2")
+@RequestMapping("/api/v2/alarm")
 public class AlarmController {
 
     private final AlarmService alarmService;
+    private final FirebaseService firebaseService;
 
     @ApiOperation("알람 로그 조회")
     @Auth
-    @GetMapping("/alarm")
+    @GetMapping("")
     public ResponseEntity<ResponseDto> alarmLog() {
         Long userId = UserContext.getCurrentUserId();
         return ResponseEntity.ok(ResponseDto.of(HttpStatus.OK, GET_ALARM_LOG, alarmService.getAlarmLog(userId)));
@@ -35,10 +39,22 @@ public class AlarmController {
 
     @ApiOperation("알람 로그 삭제 (alarmStatus: 1(전체 알람), 2(친구 알람))")
     @Auth
-    @DeleteMapping("/alarm/{alarmId}")
+    @DeleteMapping("/{alarmId}")
     public ResponseEntity<ResponseDto> deleteAlarm(@PathVariable Long alarmId, @RequestParam("alarm_status") int alarmStatus) {
         Long userId = UserContext.getCurrentUserId();
         alarmService.deleteAlarm(alarmId, userId, alarmStatus);
         return ResponseEntity.ok(ResponseDto.of(HttpStatus.OK, DELETE_ALARM_LOG));
+    }
+
+    @ApiOperation("푸시 알림 테스트")
+    @PostMapping("/test")
+    public ResponseEntity<ResponseDto> alarmTest(@RequestBody NotificationTestDto requestDto) {
+        NotificationForOneDto notificationForOne = NotificationForOneDto.builder()
+                .title(requestDto.getTitle())
+                .message(requestDto.getMessage())
+                .deviceToken(requestDto.getDeviceToken())
+                .build();
+        firebaseService.sendByTokenForOne(notificationForOne);
+        return ResponseEntity.ok(ResponseDto.of(HttpStatus.OK, "푸시 알림 보냄"));
     }
 }
---
+++ src/main/java/com/yapp/ios1/dto/notification/NotificationTestDto.java
@@ -0,0 +1,13 @@
+package com.yapp.ios1.dto.notification;
+
+import lombok.Getter;
+
+/**
+ * created by ayoung 2021/07/02
+ */
+@Getter
+public class NotificationTestDto {
+    private String title;
+    private String message;
+    private String deviceToken;
+}
